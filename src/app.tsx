03ad0561c9acbc8f992b967a9d41d1112663efb6
Eric Bryant <oniman@gmail.com>
2025-03-18 12:57:25 -0400

    nicer

--- src/app.tsx
+++ src/app.tsx
@@ -1,16 +1,14 @@
 import React, { useState, useEffect } from 'react'
 import Paper from '@mui/material/Paper'
 import Grid2 from '@mui/material/Grid2'
-import TextField from '@mui/material/TextField'
-import Button from '@mui/material/Button'
-import InputAdornment from '@mui/material/InputAdornment'
 import Typography from '@mui/material/Typography'
-
 
 export function App() {
     return (
-        <Grid2>
-            <Typography>Hello, World</Typography>
-        </Grid2>
+        <Paper variant='outlined'>
+            <Grid2 margin={5} padding={5}>
+                <Typography align='center'>Hello, World</Typography>
+            </Grid2>
+        </Paper>
     )
 }
